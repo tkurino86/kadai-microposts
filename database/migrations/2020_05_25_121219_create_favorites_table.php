edadb56abb82e206b67bd0e29727cae7132a0ccc
tadashi.kurino <t.kurino@gmail.com>
2020-05-27 12:18:11 +0000

    comment fix

--- database/migrations/2020_05_25_121219_create_favorites_table.php
+++ database/migrations/2020_05_25_121219_create_favorites_table.php
@@ -23,7 +23,7 @@ class CreateFavoritesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
 
-            // user_idとfollow_idの組み合わせの重複を許さない
+            // user_idとmicropost_idの組み合わせの重複を許さない
             $table->unique(['user_id', 'micropost_id']);
         });
     }
